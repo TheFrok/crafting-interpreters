eb5ec064431eda6b4252c5f71d34eaa6ebebce4f
TheFrok <yottam@gmail.com>
2023-09-05 12:45:10 +0300

    suspense build script
    no waiting at the end for user input

--- build_java.sh
+++ build_java.sh
@@ -1,4 +1,5 @@
-javac -cp java -d build java/com/craftinginterpreters/tool/*java || exit
-java -cp build com.craftinginterpreters.tool.GenerateAst java/com/craftinginterpreters/lox || exit
-javac -cp java -d build java/com/craftinginterpreters/lox/*java || exit
-echo "Build Done!"
+javac -cp java -d build java/com/craftinginterpreters/tool/*java  && \
+java -cp build com.craftinginterpreters.tool.GenerateAst java/com/craftinginterpreters/lox  && \
+javac -cp java -d build java/com/craftinginterpreters/lox/*java 
+read -n 1 -p "Build Done! press [Enter]"
+
