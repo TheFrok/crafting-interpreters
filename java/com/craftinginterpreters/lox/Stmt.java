9cc8c9caf702651c5af262e9e89acb874b947f5d
TheFrok <yottam@gmail.com>
2024-04-27 14:32:02 +0300

    Added loops, and functions

--- java/com/craftinginterpreters/lox/Stmt.java
+++ java/com/craftinginterpreters/lox/Stmt.java
@@ -6,7 +6,10 @@ abstract class Stmt {
   interface Visitor<R> {
     R visitBlockStmt(Block stmt);
     R visitExpressionStmt(Expression stmt);
+    R visitFunctionStmt(Function stmt);
+    R visitIfStmt(If stmt);
     R visitPrintStmt(Print stmt);
+    R visitWhileStmt(While stmt);
     R visitVarStmt(Var stmt);
   }
   static class Block extends Stmt {
@@ -33,6 +36,38 @@ abstract class Stmt {
 
     final Expr expression;
   }
+  static class Function extends Stmt {
+    Function(Token name, List<Token> params, List<Stmt> body) {
+      this.name = name;
+      this.params = params;
+      this.body = body;
+    }
+
+    @Override
+    <R> R accept(Visitor<R> visitor) {
+      return visitor.visitFunctionStmt(this);
+    }
+
+    final Token name;
+    final List<Token> params;
+    final List<Stmt> body;
+  }
+  static class If extends Stmt {
+    If(Expr condition, Stmt thenBranch, Stmt elseBranch) {
+      this.condition = condition;
+      this.thenBranch = thenBranch;
+      this.elseBranch = elseBranch;
+    }
+
+    @Override
+    <R> R accept(Visitor<R> visitor) {
+      return visitor.visitIfStmt(this);
+    }
+
+    final Expr condition;
+    final Stmt thenBranch;
+    final Stmt elseBranch;
+  }
   static class Print extends Stmt {
     Print(Expr expression) {
       this.expression = expression;
@@ -44,6 +79,20 @@ abstract class Stmt {
     }
 
     final Expr expression;
+  }
+  static class While extends Stmt {
+    While(Expr condition, Stmt body) {
+      this.condition = condition;
+      this.body = body;
+    }
+
+    @Override
+    <R> R accept(Visitor<R> visitor) {
+      return visitor.visitWhileStmt(this);
+    }
+
+    final Expr condition;
+    final Stmt body;
   }
   static class Var extends Stmt {
     Var(Token name, Expr initializer) {
